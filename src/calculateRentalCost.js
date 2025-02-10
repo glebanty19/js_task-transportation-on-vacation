3f8a87fb2f873f15f68e959ea0b3cb20765cbca2
Hleb Bondar <hlebbondar2004@gmail.com>
2025-02-10 18:50:01 +0200

    correction the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,23 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const cost = 40;
-  const DAY_WITHOUT_DISCOUNT = 2;
+  const costPerDay = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
+  const MIDDLE_TERM = 3;
   const MIDDLE_TERM_DISCOUNT = 20;
 
-  if (days <= DAY_WITHOUT_DISCOUNT) {
-    return days * cost;
-  }
-
-  if (days < LONG_TERM) {
-    return days * cost - MIDDLE_TERM_DISCOUNT;
-  }
+  const basePrice = days * costPerDay;
 
   if (days >= LONG_TERM) {
-    return days * cost - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
+
+  if (days >= MIDDLE_TERM) {
+    return basePrice - MIDDLE_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
